5cd57db90dc006311266f4a35cb99db27f8efb4b
matheusbruns <matheus.rbruns@gmail.com>
2023-08-06 15:33:31 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/dev/hrpayroll/entities/WorkerModel.java
@@ -0,0 +1,12 @@
+package com.dev.hrpayroll.entities;
+
+import lombok.Data;
+
+@Data
+public class WorkerModel {
+
+	private long id;
+	private String name;
+	private Double dailyIncome;
+
+}
--- hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/dev/hrpayroll/services/PaymentService.java
@@ -1,14 +1,31 @@
 package com.dev.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.dev.hrpayroll.entities.PaymentModel;
+import com.dev.hrpayroll.entities.WorkerModel;
 
 @Service
 public class PaymentService {
-	
-	public PaymentModel getPayment(Long workerId, Integer days ) {
-		return new PaymentModel("bob", 200.0, days);
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	public PaymentModel getPayment(Long workerId, Integer days) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("workerId", workerId.toString());
+		WorkerModel worker = restTemplate.getForObject(workerHost + "/workers/find-by-id?workerId={workerId}",
+				WorkerModel.class, uriVariables);
+		return new PaymentModel(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
